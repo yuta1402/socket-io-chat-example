43ea38dc364d58a681ae93989eb662fb566ffd0d
gut <yuta1402souja@live.jp>
2020-06-18 00:18:08 +0900

    Support socket.io connected

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("connected");
+});
+
+server.listen(PORT, () => {
   console.log("server listening on port " + PORT);
 });
